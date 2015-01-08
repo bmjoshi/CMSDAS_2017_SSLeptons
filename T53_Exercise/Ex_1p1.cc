f33cc1176fe1ea408f517bee7b256bad0d7cfe5e
clint-richardson <clint.a.richardson@gmail.com>
2015-01-08 09:49:16 -0500

    added parametrization vs eta, pt

--- T53_Exercise/Ex_1p1.cc
+++ T53_Exercise/Ex_1p1.cc
@@ -43,4 +43,40 @@ void Ex_1p1(){
   ssmass->Draw();
   
   c3.Print("DiElectronInvariantMass_ss.pdf");
+
+  //Now draw fake rate vs eta, pt
+
+  //define cuts
+  string masscuts = "76 < diElMass_DileptonCalc < 111";
+  string SSmasscuts = "(76 < diElMass_DileptonCalc < 111) && (elCharge1_DileptonCalc == elCharge2_DileptonCalc)";
+
+  //first get histograms
+  TH1F* ssEtaHist = new TH1F("ssEtaHist","#eta",30,-3,3);
+  TH1F* totEtaHist = new TH1F("totEtaHist","#eta",30,-3,3);
+
+  t->Draw("elEta_DileptonCalc>>ssEtaHist",SSmasscuts.c_str());
+  t->Draw("elEta_DileptonCalc>>totEtaHist",masscuts.c_str());
+
+  //make new TGraph to get ratio
+  TGraphAsymmErrors* etaGraph = new TGraphAsymmErrors(ssEtaHist,totEtaHist);
+
+  TCanvas c5;
+  etaGraph->Draw("apl");
+  c5.Print("chargeMisID_vEta.pdf");
+
+  //same as above but for pt
+  //first get histograms
+  TH1F* ssPtHist = new TH1F("ssPtHist","p_{T}",100,0.,200.);
+  TH1F* totPtHist = new TH1F("totPtHist","p_{T}",100,0.,200.);
+
+  t->Draw("elPt_DileptonCalc>>ssPtHist",SSmasscuts.c_str());
+  t->Draw("elPt_DileptonCalc>>totPtHist",masscuts.c_str());
+
+  //make new TGraph to get ratio
+  TGraphAsymmErrors* ptGraph = new TGraphAsymmErrors(ssPtHist,totPtHist);
+
+  TCanvas c5;
+  ptGraph->Draw("apl");
+  c5.Print("chargeMisID_vPt.pdf");
+
 }
