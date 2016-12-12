53c9b06c8689a90b6bccba7d6e9d71eabe7a0384
David <dzou@bu.edu>
2016-12-12 13:27:59 -0600

    Changes to Ex_1p1 to use new input files.

--- X53_Exercise/Ex_1p1.cc
+++ X53_Exercise/Ex_1p1.cc
@@ -21,52 +21,30 @@ void Ex_1p1(){
   */
 
   //load file and tree
-  TChain* t = new TChain("ljmet");
-  t->Add("/uscms_data/d3/clint/public/ljmet_tree_DY.root");
-  //t->Add("/uscms_data/d3/clint/public/ljmet_tree_TT1.root");
+  //TChain* t = new TChain("ljmet");
+  TChain* t = new TChain("ChargeMisID");
+  t->Add("ChargeMisID_Data_Run2016_Electrons_MVATightRC_100events.root");
   
 
   int nEntries = t->GetEntries();
   //kinematic variables
-  vector<double> *elPts = 0;
-  vector<double> *elEtas =0;
-  vector<double> *elPhis =0;
 
-  vector<int>* elCharge = 0;
-
-  //variables for tracking cuts
-  vector<double> *elDeta =0;
-  vector<double> *elDphi =0;
-  //variables for primary vtx cuts
-  vector<double> *elDZs = 0;
-  vector<double> *elD0s = 0;
-  // H over E
-  vector<double> *elHoverEs = 0;
-  //missing hits
-  vector<int> *elMHits = 0;
-  //ooemoop
-  vector<double> *elOoEmooPs = 0;
-  //charged isolation
-  vector<double> *elRelIsos = 0;
-  //sigmaIetaIeta
-  vector<double>* elSigmaIetaIetas = 0;
-  vector<int>* elChargeConsistency = 0;
+  float elPts1, elEtas1, elPhis1, elEs1;
+  float elPts2, elEtas2, elPhis2, elEs2;
+  int elCharge1, elCharge2;
+  int lep1etabin, lep2etabin;
 
   //Set branch addresses
-  t->SetBranchAddress("elPt_DileptonCalc", &elPts);
-  t->SetBranchAddress("elEta_DileptonCalc", &elEtas);
-  t->SetBranchAddress("elPhi_DileptonCalc", &elPhis);
-  t->SetBranchAddress("elCharge_DileptonCalc", &elCharge);
-  t->SetBranchAddress("elDeta_DileptonCalc", &elDeta);
-  t->SetBranchAddress("elDphi_DileptonCalc", &elDphi);
-  t->SetBranchAddress("elDZ_DileptonCalc", &elDZs);
-  t->SetBranchAddress("elD0_DileptonCalc", &elD0s);
-  t->SetBranchAddress("elHoE_DileptonCalc",&elHoverEs);
-  t->SetBranchAddress("elMHits_DileptonCalc",&elMHits);
-  t->SetBranchAddress("elOoemoop_DileptonCalc",&elOoEmooPs);
-  t->SetBranchAddress("elRelIso_DileptonCalc",&elRelIsos);
-  t->SetBranchAddress("elSihih_DileptonCalc",&elSigmaIetaIetas);
-  t->SetBranchAddress("elChargeConsistent_DileptonCalc",&elChargeConsistency);
+  t->SetBranchAddress("Lep1Pt", &elPts1);
+  t->SetBranchAddress("Lep1Eta", &elEtas1);
+  t->SetBranchAddress("Lep1Phi", &elPhis1);
+  t->SetBranchAddress("Lep1E", &elEs1);
+  t->SetBranchAddress("Lep1Charge", &elCharge1);
+  t->SetBranchAddress("Lep2Pt", &elPts2);
+  t->SetBranchAddress("Lep2Eta", &elEtas2);
+  t->SetBranchAddress("Lep2Phi", &elPhis2);
+  t->SetBranchAddress("Lep2E", &elEs2);
+  t->SetBranchAddress("Lep2Charge", &elCharge2);
 
   //Histograms
   TH1F* allmass = new TH1F("allmass","DiElectron Invariant Mass All Events",100,0.,200.);
@@ -83,67 +61,32 @@ void Ex_1p1(){
   for(int ient = 0; ient < nEntries; ient++){
     t->GetEntry(ient);
     if(ient % 1000 ==0) std::cout<<"Completed "<<ient<<" out of "<<nEntries<<" events"<<std::endl;
-    //Put electrons back together into coherent objects
-    vector <Electron*> vEl;
-    for (unsigned int uiEl = 0; uiEl < elPts->size(); uiEl++){
-      Electron* el = new Electron;
-      
-      el->pt                = elPts->at(uiEl);
-      el->eta               = elEtas->at(uiEl);
-      el->phi               = elPhis->at(uiEl);
-      el->charge            = elCharge->at(uiEl);
-      el->dEta              = elDeta->at(uiEl);
-      el->dPhi              = elDphi->at(uiEl);
-      el->dZ                = elDZs->at(uiEl);
-      el->d0                = elD0s->at(uiEl);
-      el->hOverE            = elHoverEs->at(uiEl);
-      el->mHits             = elMHits->at(uiEl);
-      el->ooEmooP           = elOoEmooPs->at(uiEl);
-      el->relIso            = elRelIsos->at(uiEl);
-      el->sigmaIetaIeta     = elSigmaIetaIetas->at(uiEl);
-      el->chargeConsistency = elChargeConsistency->at(uiEl);
 
-      vEl.push_back(el);
-    }
+    TLorentzVector v1, v2;
+    v1.SetPtEtaPhiM(elPts1, elEtas1, elPhis1, M_EL);
+    v2.SetPtEtaPhiM(elPts2, elEtas2, elPhis2, M_EL);
+    double mass = (v1+v2).M();
 
-    bool foundPair = false;
-    for(unsigned int ui = 0; ui < vEl.size(); ui++){
-      // ADD CODE HERE - THE LINE BELOW DETERMINES THE QUALITY OF THE ELECTRON
-      if (!vEl.at(ui)->tight()) continue;
-      for(unsigned int uj = ui + 1; uj < vEl.size(); uj++){
-	if (!vEl.at(uj)->tight()) continue;
+    allmass->Fill(mass);
 
-	TLorentzVector v1, v2;
-	v1.SetPtEtaPhiM(vEl.at(ui)->pt, vEl.at(ui)->eta, vEl.at(ui)->phi, M_EL);
-	v2.SetPtEtaPhiM(vEl.at(uj)->pt, vEl.at(uj)->eta, vEl.at(uj)->phi, M_EL);
-	
-	double mass = (v1+v2).M();
-	if (mass > M_Z - dM && mass < M_Z + dM){
-	  foundPair = true;
-	  allmass->Fill(mass);
-
-	  totEtaHist->Fill(v1.Eta());
-	  totEtaHist->Fill(v2.Eta());
-
-	  totPtHist->Fill(v1.Pt());
-	  totPtHist->Fill(v2.Pt());
+    totEtaHist->Fill(v1.Eta());
+    totEtaHist->Fill(v2.Eta());
 	  
-	  if (vEl.at(ui)->charge == vEl.at(uj)->charge){
-	    ssmass->Fill(mass);
-	    
-	    ssEtaHist->Fill(v1.Eta());
-	    ssEtaHist->Fill(v2.Eta());
-	    
-	    ssPtHist->Fill(v1.Pt());
-	    ssPtHist->Fill(v2.Pt());
-	  }
-	  else osmass->Fill(mass);
-	}	
-      }//End loop over second lepton
-      if (foundPair) break;
-    }//End loop over first lepton
+    totPtHist->Fill(v1.Pt());
+    totPtHist->Fill(v2.Pt());
 
-  }//End loop over tree  
+    //if (elPts1->at(uiEl) == elPts2->at(uiEl)){
+    if (elCharge1 == elCharge2 ){
+      ssmass->Fill(mass);	    
+      ssEtaHist->Fill(v1.Eta());
+      ssEtaHist->Fill(v2.Eta());
+      ssPtHist->Fill(v1.Pt());
+      ssPtHist->Fill(v2.Pt());
+    }
+    else{
+      osmass->Fill(mass);
+    }
+  }
 
   //Draw the histograms
   TCanvas c1;
@@ -163,8 +106,7 @@ void Ex_1p1(){
   TCanvas c3;
   ssmass->Draw();
   
-  c3.Print("DiElectronInvariantMass_ss.pdf");
-  
+  c3.Print("DiElectronInvariantMass_ss.pdf");  
 
 
   //ADD CODE TO PRINT OUT CHARGE MISID RATE
@@ -173,7 +115,8 @@ void Ex_1p1(){
   std::cout<<"fake rate: "<< ssmass->Integral() /  allmass->Integral()<<std::endl;
 
   /* ADD CODE HERE TO MAKE PLOTS OF THE CHARGE MISID RATE VS PT, ETA. SOME OF IT IS THERE SO THAT WE ALL HAVE THE SAME OUTPUT FILES
-     HINT: MAKE USE OF TGRAPHASYMMERRORS
+     HINT1: MAKE USE OF TGRAPHASYMMERRORS, 
+     HINT2: SET YOUR VS ETA BINS ACCORDING TO THE getEtaBin METHOD PROVIDED AT THE BOTTOM OF THIS FILE
    */
   //tcanvas for eta plot
   TCanvas c4;
@@ -190,4 +133,16 @@ void Ex_1p1(){
 
   c5.Print("chargeMisID_vPt.pdf");
 
+}
+
+int getEtaBin(float abseta){
+  int bin=-1;
+  if(abseta>2.0) bin=6;
+  else if(abseta>1.556) bin=5;
+  else if(abseta>1.442) bin=4;
+  else if(abseta>0.8) bin=3;
+  else if(abseta>0.4) bin=2;
+  else if(abseta>=0.0) bin=1;
+
+  return bin;
 }
